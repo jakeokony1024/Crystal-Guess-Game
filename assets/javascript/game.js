efd7ed8d0dee3ee650a6a33b21fcc29e2a4fbfb3
Jakeokony1024 <jakeokony@gmail.com>
2020-01-05 20:12:52 -0600

    added loss and win counter and reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,8 @@ var targetNumber = Math.floor((Math.random() * 120) + 1);
   $("#numberToGuess").text(targetNumber);
 
   var counter = 0;
-  var winCounter =  new Array;
-  var lossCounter = new Array;
+  var winCounter = 0;
+  var lossCounter = 0;
   var numberOptions = [1, 7, 3, 12];
 
   
@@ -22,34 +22,35 @@ var targetNumber = Math.floor((Math.random() * 120) + 1);
     $("#crystals").append(imageCrystal);
   }
 
-
-
-  
+  // Function to reset the number to guess and the counter//
+  function reset(){
+    counter = 0;
+    targetNumber = Math.floor((Math.random() * 120) + 1);
+    $("#numberToGuess").text(targetNumber);
+  }
+  // Function to get value of crystal clicked and add it to counter//
   $(".crystal-image").on("click", function() {
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
+    
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
     $("#collected").text(" Crystals Collected: " + counter);
+
+  // sets alert for winning and adds to the win counter
 
     if (counter === targetNumber) {
       alert("You win!");
-      $("#wins").text(" Wins: " + winCounter +1);
+      $("#wins").text(" Wins: " + winCounter++);
+      reset();
+      
     }
-
+  // sets alert for losing and adds to the loss counter
     else if (counter >= targetNumber) {
       alert("You lose!!");
-      $("#losses").text("Losses: " + lossCounter +1);
-
+      $("#losses").text("Losses: " + lossCounter++);
+      reset();
+      
     }
 
   });
